0ffcfdb0830e5e1c50f8d7c36e8f7fb743ff8f0c
Ji Chang <ji.chang@shopee.com>
2019-01-11 17:12:27 +0800

    add project Feblr.RobotsTxt for parsing robots.txt

---
+++ src/Feblr.RobotsTxt/Library.fs
@@ -0,0 +1,142 @@
+namespace Feblr
+
+open System
+
+module RobotsTxt =
+    type UserAgent =
+        | Allbot
+        | GoogleBot
+        | BingBot
+        | BaiduSpider
+        | Unrecognize of string
+
+    type Directive =
+        | Allow of string
+        | Disallow of string
+        | Unsupport of string
+
+    type Entry =
+        { userAgents: UserAgent list 
+          directives: Directive list }
+
+        static member empty =
+            { userAgents = List.empty; directives = List.empty }
+
+        static member isEmpty (entry: Entry) =
+            List.isEmpty entry.userAgents && List.isEmpty entry.directives
+
+    type Sitemap = string
+
+    type RobotsTxt =
+        { entries: Entry list
+          sitemap: Sitemap list }
+
+        static member empty =
+            { entries = List.empty; sitemap = List.empty }
+
+        static member isAllowed (robotsTxt: RobotsTxt) (userAgent: UserAgent) (pathname: string) : bool =
+            let isMatch (basePath: string) (targetPath: string) : bool =
+                 if basePath = targetPath then
+                     true
+                 else
+                     targetPath.StartsWith(basePath) && targetPath.[basePath.Length] = '/'
+
+            let checkDirective (allowed: bool) (directive: Directive) =
+                match directive with
+                | Allow allowPathname ->
+                    if isMatch allowPathname pathname then true else allowed
+                | Disallow disallowPathname ->
+                    if isMatch disallowPathname pathname then false else allowed
+                | _ ->
+                    allowed
+
+            let checkEntry allowed entry =
+                match List.tryFind (fun _userAgent -> _userAgent = Allbot || _userAgent = userAgent) entry.userAgents with
+                | Some _ ->
+                    List.fold checkDirective allowed entry.directives
+                | _ ->
+                    allowed
+
+            List.fold checkEntry true robotsTxt.entries
+
+    type Line =
+        | Empty of string
+        | Comment of string
+        | UserAgent of UserAgent
+        | Directive of Directive
+        | Sitemap of Sitemap
+        | Illegal of string
+
+    let parseLine (line: string) : Line =
+        if String.IsNullOrWhiteSpace line then
+            Empty line
+        else
+            if line.StartsWith("#") then
+                Comment line
+            else
+                let parts = line.Split([|':'|])
+                let firstPart = parts.[0].ToLower()
+                let commentStartPosition = parts.[1].IndexOf("#")
+                let secondPart =
+                    if commentStartPosition = -1 then
+                        parts.[1].Trim()
+                    else
+                        parts.[1].Substring(0, commentStartPosition).Trim()
+                if parts.Length = 2 then
+                    match firstPart with
+                    | "user-agent" ->
+                        let userAgent =
+                            match secondPart.ToLower() with
+                            | "*" -> Allbot
+                            | "googlebot" -> GoogleBot
+                            | "bingbot" -> BingBot
+                            | "baiduspider" -> BaiduSpider
+                            | _ -> Unrecognize secondPart
+                        UserAgent userAgent
+                    | "allow" ->
+                        let directive = Allow secondPart
+                        Directive directive
+                    | "disallow" ->
+                        let directive = Disallow secondPart
+                        Directive directive
+                    | "sitemap" ->
+                        Sitemap secondPart
+                    | _ ->
+                        let directive = Unsupport line
+                        Directive directive
+                else
+                    Illegal line
+
+    let groupLine (robotsTxt: RobotsTxt) (line: Line) =
+        let firstEntry = List.head robotsTxt.entries
+        let restEntries = List.tail robotsTxt.entries
+        match line with
+        | Illegal line
+        | Empty line
+        | Comment line ->
+            robotsTxt
+        | UserAgent userAgent ->
+            if List.isEmpty firstEntry.directives then
+                let newEntry = { firstEntry with userAgents = List.append firstEntry.userAgents [userAgent] }
+                { robotsTxt with entries = newEntry :: restEntries }
+            else
+                let newEntry = { Entry.empty with userAgents = [userAgent] }
+                { robotsTxt with entries = newEntry :: robotsTxt.entries }
+        | Directive directive ->
+            let newEntry = { firstEntry with directives = List.append firstEntry.directives [directive] }
+            { robotsTxt with entries = newEntry :: restEntries }
+        | Sitemap sitemap ->
+            { robotsTxt with sitemap = List.append robotsTxt.sitemap [sitemap] }
+
+    let removeEmptyEntry (robotsTxt: RobotsTxt) : RobotsTxt =
+        let newEntries =
+            List.filter (not << Entry.isEmpty) robotsTxt.entries
+
+        { robotsTxt with entries = List.rev newEntries }
+
+    let parse (txt: string): RobotsTxt =
+        txt.Split([|'\n'|])
+        |> Seq.filter (not << String.IsNullOrWhiteSpace)
+        |> Seq.map parseLine
+        |> Seq.fold groupLine { RobotsTxt.empty with entries = [ Entry.empty ] }
+        |> removeEmptyEntry
---
+++ tests/Feblr.RobotsTxt.Tests/Main.fs
@@ -0,0 +1,45 @@
+﻿module Feblr.RobotsTxt.Tests
+
+open Expecto
+open Feblr.RobotsTxt
+open Feblr
+
+let robotsTxtMock = """
+User-agent: Googlebot
+Allow: /hello
+Disallow: /world
+Allow: /comment #comment
+Sitemap: /sitemap
+"""
+
+[<Tests>]
+let tests =
+  testList "RobotsTxt" [
+    testCase "empty string should return RobotsTxt.empty" <| fun _ ->
+      let robotsTxt = RobotsTxt.parse ""
+      Expect.equal robotsTxt RobotsTxt.empty "should return empty robostxt"
+    testCase "mock string should return expected RobotsTxt" <| fun _ ->
+      let robotsTxt = RobotsTxt.parse robotsTxtMock
+      let entry =
+          { userAgents = [ GoogleBot ]
+            directives = [ Allow "/hello"; Disallow "/world"; Allow "/comment" ] }
+      let expected =
+          { entries = [ entry ]
+            sitemap = ["/sitemap"] }
+      Expect.equal robotsTxt expected "should return expected state"
+    testCase "match should return expected state" <| fun _ ->
+      let robotsTxt = RobotsTxt.parse robotsTxtMock
+      let allowHello = RobotsTxt.isAllowed robotsTxt GoogleBot "/hello"
+      let allowWorld2 = RobotsTxt.isAllowed robotsTxt GoogleBot "/world2"
+      let allowWorld = RobotsTxt.isAllowed robotsTxt GoogleBot "/world"
+      let allowComment = RobotsTxt.isAllowed robotsTxt GoogleBot "/comment"
+      Expect.equal allowHello true "should return expected state"
+      Expect.equal allowWorld2 true "should return expected state"
+      Expect.equal allowWorld false "should return expected state"
+      Expect.equal allowComment true "should return expected state"
+  ]
+
+
+[<EntryPoint>]
+let main argv =
+    Tests.runTestsInAssembly defaultConfig argv
